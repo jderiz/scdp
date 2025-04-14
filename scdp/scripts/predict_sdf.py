3bed9ecc05097bb1b386abac92daf9c3bc24ed1f
Paul Eisenhuth <eisenhuth@informatik.uni-leipzig.de>
2025-04-14 17:11:42 +0200

    Added rotation code for equivariance testing

--- scdp/scripts/predict_sdf.py
+++ scdp/scripts/predict_sdf.py
@@ -13,16 +13,15 @@ from scdp.common.pyg import DataLoader
 
 z_table = get_atomic_number_table_from_zs(np.arange(100).tolist())
 
-#metadata='caffeine'
-#mol_file='caffeine.sdf'
+metadata='caffeine'
+mol_file='caffeine.sdf'
 
-
-metadata='test'
-mol_file='/home/iwe20/Projects/scdp/experiments/1.001_100.sdf'
+#metadata='test'
+#mol_file='/home/iwe20/Projects/scdp/experiments/1.001_100.sdf'
 
 device = 'cuda'
 dimensional_padding = 2.0
-resolution = 0.1
+resolution = 0.25
 
 bohr = 1.88973
 
@@ -50,6 +49,16 @@ for i in range(3):
 atom_types = torch.tensor( mol_atom_types )
 # positions in angstrom
 atom_coords = torch.tensor(mol_pos)
+
+rotation = torch.tensor(
+    [[ 1, 0, 0],
+     [ 0, 0,-1],
+     [ 0, 1, 0]],
+     dtype=torch.float
+)
+
+atom_coords_rot = atom_coords @ rotation
+
 # base vector of coordinate system
 cube_length = [ max_edge[i]-origin[i] for i in range(3) ]
 cell = torch.tensor([
@@ -87,6 +96,22 @@ data_object = AtomicData.build_graph_with_vnodes(
                 max_neighbors=None,
                 device=device,
             )
+data_object_rot = AtomicData.build_graph_with_vnodes(
+                atom_coords=atom_coords_rot,
+                atom_types=atom_types,
+                cell=cell,
+                chg_density=chg_density,
+                origin=origin,
+                metadata=metadata + '_rot',
+                z_table=z_table,
+                atom_cutoff=6.0,
+                vnode_method='none',
+                vnode_factor=3,
+                vnode_res=0.8,
+                disable_pbc=True,
+                max_neighbors=None,
+                device=device,
+            )
 print("Created AtomicData object")
 
 class mol_data( Dataset ):
@@ -100,7 +125,7 @@ class mol_data( Dataset ):
     def __getitem__(self, index):
         return self.atom_data[index]
 
-md = mol_data( [data_object] )
+md = mol_data( [data_object, data_object_rot] )
 
 model = ChgLightningModule.load_from_checkpoint(checkpoint_path= 'qm9_none_K4L3_beta_2.0/epoch=59-step=464400.ckpt').to(device)
 model.eval()
@@ -111,21 +136,29 @@ pytorch_total_params = sum(p.numel() for p in model.parameters())
 print("Loaded model to", device)
 print("\tParameters:", pytorch_total_params)
 
-loader = DataLoader( md )
+loader = DataLoader( md, batch_size=1 )
+
+coeffs_arr = []
+
+print(model.gto_dict)
+print(model.gto_dict['1'])
 
 with( torch.no_grad() ):
     for batch in loader:
+        print('Start processing', batch.metadata[0])
         batch = batch.to(device)
         coeffs, expo_scaling = model.predict_coeffs(batch)
+        coeffs_arr.append( coeffs )
         print('Calculated', coeffs.shape[1], 'coefficients each for', coeffs.shape[0], 'atoms')
         print('\tExponent scaling:', expo_scaling.shape if expo_scaling != None else 'None')
+        np.savetxt(batch.metadata[0] + '.coeffs', coeffs.cpu().numpy())
 
         pred = model.orbital_inference( batch, coeffs, expo_scaling, batch.n_probe, batch.probe_coords )
         print( 'Calculated densities for', batch.n_probe.item(), 'probes' )
 
         print('Start writing cube file...')
-        with open( metadata + '.cube', 'w') as file:
-            string = metadata + '\n' + 'Created by Paul and scdp\n'
+        with open( batch.metadata[0] + '.cube', 'w') as file:
+            string = batch.metadata[0] + '\n' + 'Created by Paul and scdp\n'
             string += f'{len(atom_types):4}{origin[0]*bohr:12.6f}{origin[1]*bohr:12.6f}{origin[2]*bohr:12.6f}\n'
             for axis in range(3):
                 string += f'{chg_dimension[axis]:4}'
@@ -143,3 +176,5 @@ with( torch.no_grad() ):
                     string += '\n'
             file.write(string)
         print('Done.')
+
+print( torch.isclose(coeffs_arr[0], coeffs_arr[1], atol=1e-2, rtol=1e-2)[0] )
